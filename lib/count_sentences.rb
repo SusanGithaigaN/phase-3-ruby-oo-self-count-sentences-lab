0a436ea312cce81ef82dae882c0141e16e04b2c6
SusanGithaigaN <susan.githaiga@student.moringaschool.com>
2023-02-09 16:00:25 +0300

    commit tree 0fc8144dc24b6e9c741212a347c7a6e5077ba7e1

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+     self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # split string, match instances of . ? !
+    # return the number of elements that results from #split
+    self.split(/[.?!]+/).count
   end
 end
+string = "This is the first sentence. This is the second sentence? And this is the third!"
+
+puts string.count_sentences
